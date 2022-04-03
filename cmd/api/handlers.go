4f2770a346a6431b8e5493be7ee871ad9588e721
Bruno Bandeira <brunoban.dev@gmail.com>
2022-04-03 15:41:20 +0100

    first commit

---
+++ cmd/api/handlers.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+}
+
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
+	type credentials struct {
+		UserName string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var creds credentials
+	var payload jsonResponse
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		// send back error message
+		app.errorLog.Println("invalid json")
+		payload.Error = true
+		payload.Message = "invalid json"
+
+		out, err := json.MarshalIndent(payload, "", "\t")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(out)
+		return
+	}
+
+	// TODO authenticate
+	app.infoLog.Println(creds.UserName, creds.Password)
+
+	// send back a response
+	payload.Error = false
+	payload.Message = "signed in"
+
+	out, err := json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		app.errorLog.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
---
+++ cmd/api/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// config is the type for all application configuration.
+type config struct {
+	port int
+}
+
+// application is the type for all data we want to share with the
+// various parts of our application. We will share this information in most
+// cases by using this type as the receiver for functions
+type application struct {
+	config   config
+	infoLog  *log.Logger
+	errorLog *log.Logger
+}
+
+// main is the main entry point for our application
+func main() {
+	var cfg config
+	cfg.port = 8081
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	err := app.serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// serve starts the web server
+func (app *application) serve() error {
+	app.infoLog.Println("API Listening on port", app.config.port)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", app.config.port),
+		Handler: app.routes(),
+	}
+
+	return srv.ListenAndServe()
+}
