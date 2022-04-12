f25af97cecd9db2423634fa6c12abd5cf25f443b
Bruno Bandeira <brunoban.dev@gmail.com>
2022-04-12 20:46:07 +0100

    adding a new user into the database

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -35,5 +35,27 @@ func (app *application) routes() http.Handler {
 		app.writeJSON(w, http.StatusOK, all)
 	})
 
+	mux.Get("/users/add", func(w http.ResponseWriter, r *http.Request) {
+		var u = data.User{
+			Email:     "you@there.com",
+			FirstName: "you",
+			LastName:  "there",
+			Password:  "password",
+		}
+
+		app.infoLog.Println("Adding user ...")
+
+		id, err := app.models.User.Insert(u)
+		if err != nil {
+			app.errorLog.Println(err)
+			app.errorJSON(w, err, http.StatusForbidden)
+			return
+		}
+
+		app.infoLog.Println("Got back id of", id)
+		newUser, _ := app.models.User.GetOne(id)
+		app.writeJSON(w, http.StatusOK, newUser)
+	})
+
 	return mux
 }
